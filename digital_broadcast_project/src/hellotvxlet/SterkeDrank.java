070ff01d67b6ce29a1aee45e88e38b5a634a9065
DriesH <dries.heyninck@gmail.com>
2016-04-20 12:47:49 +0200

    Observer, subject en main class added

---
+++ digital_broadcast_project/src/hellotvxlet/SterkeDrank.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hellotvxlet;
+
+import java.awt.Color;
+import java.awt.Image;
+import java.awt.MediaTracker;
+import org.dvb.ui.DVBColor;
+import org.havi.ui.HStaticText;
+import org.havi.ui.HTextButton;
+import org.havi.ui.HVisible;
+/**
+ *
+ * @author student
+ */
+public class SterkeDrank implements ObserverInterface {
+    
+    
+
+}
